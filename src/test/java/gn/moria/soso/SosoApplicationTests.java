da0bffdf223c1c11ade7e0e57194d234b0f7c558
kukufuckingkan <keitakukukhan@gmail.com>
2024-10-18 23:14:15 +0000

    sdsds

---
+++ src/test/java/gn/moria/soso/SosoApplicationTests.java
@@ -0,0 +1,13 @@
+package gn.moria.soso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SosoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
